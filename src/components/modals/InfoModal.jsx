6cf8e5e137537a1e6080722869643264220f9b62
jeliaaa <julia.aleqsandre@gmail.com>
2025-12-22 22:06:44 +0400

    a

--- src/components/modals/InfoModal.jsx
+++ src/components/modals/InfoModal.jsx
@@ -29,21 +29,26 @@ const InfoModal = () => {
                     <div onClick={() => setIsOpen(false)} className='bg-black/30 z-50 w-dvw h-dvh absolute'></div>
                     <div className='z-51 w-3/4 h-3/4 bg-white rounded-4xl flex flex-col'>
                         <div className='w-full bg-indigo-600 rounded-t-4xl border-white border-b-2 py-7 px-5 flex justify-between items-center'>
-                            <span className='text-2xl text-white font-bold'>გასათვალისწინებელი ინფორმაცია</span>
+                            <span className='text-2xl text-white font-bold'></span>
                             <X className='hover:rotate-90 hover:font-bold transition-all delay-200 text-white cursor-pointer' onClick={() => setIsOpen(false)} />
                         </div>
                         <div id='info-cont' className='w-full overflow-y-auto p-5 flex flex-col gap-y-5'>
                             <div>
-                                <h1 className='text-2xl'>დისქლეიმერი</h1>
+                                <div className='w-full flex flex-wrap gap-5 items-center'>
+                                    <img src={partner1} className='h-15' />
+                                    <img src={partner2} className='h-25' />
+                                    <img src={partner3} className='h-25' />
+
+                                </div>
                                 <p className='mt-5'>
-                                    ეს რუკა შექმნილია ევროკავშირის მხარდაჭერით. მის შინაარსზე სრულად პასუხისმგებელია ორგანიზაცია ,,ბათუმის დამოუკიდებელი ცხოვრების ცენტრი’’ (BILC) და შესაძლოა, რომ იგი არ გამოხატავდეს ევროკავშირის შეხედულებებს.
+                                    აჭარის რეგიონში ევროკავშირის პროექტების შესახებ საინფორმაციო რუკა. პროექტი “ერთიანი საქართველო ევროპისთვის” მხარდაჭერილია ევროკავშირის მიერ, მას ახორციელებს კონსორციუმი კონრად ადენაუერის ფონდის (KAS) ხელმძღვანელობით, რომელშიც შედიან ეკონომიკური პოლიტიკის კვლევის ცენტრი (EPRC) სამოქალაქო საზოგადოების ფონდი (CSF) და თავისუფალ ჟურნალისტთა სახლი (IJH). პროექტის მიზანია ევროკავშირის შესახებ ცნობიერების ამაღლება.
                                 </p>
                             </div>
                             <div>
-                                <h1 className='text-2xl'>პარტნიორი ორგანიზაციები</h1>
+                                <h1 className='text-2xl'>დისქლეიმერი</h1>
                                 <div className='flex flex-wrap items-center gap-4 mt-5'>
-                                    {partners.map((p, i) => (
-                                        <img className={`${[1,2].includes(i) ? "h-25" : "h-15"}`} key={i} src={p}/>
+                                    {partners.slice(3,partners.length).map((p, i) => (
+                                        <img className="h-25" key={i} src={p} />
                                     ))}
                                 </div>
                             </div>
